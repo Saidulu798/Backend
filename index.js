756474e990b82def621dee048670dafad7544220
unknown <mahiroczzs@gmail.com>
2024-05-11 18:02:09 +0530

    first commit

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const PORT= process.env.PORT
 app.listen(PORT,()=>{
     console.log(`Server Running on the PORT ${PORT}`)
 })
-const DB_URL = process.env.CONNECTION_URL
+const DB_URL = process.env.MONGO_URL
 
 mongoose.connect(DB_URL,{useNewUrlParser: true,useUnifiedTopology: true}).then(()=>{
     console.log("MongoDB database connected")
